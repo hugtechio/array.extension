40059038a42f32b6f20f30e22696dc58d8c99d92
haruharuharuby <cocktail.house@gmail.com>
2019-10-15 17:56:45 +0200

    tsc

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,9 +3,13 @@ declare global {
     interface Array<T> {
         uniq<T>(comparer: (value1: T, valu2: T) => boolean): T[];
         contains<T>(value: T): boolean;
-        findExactOne<T>(matcher: (value: T) => boolean): T | null;
     }
     interface Object {
         has(key: string): boolean;
+        filterKeys(keys: string[]): Object;
+        indexed(): IndexObject;
     }
+}
+interface IndexObject {
+    [index: string]: string;
 }
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
 /* eslint no-extend-native: ["error", { "exceptions": ["Array","Object"] }] */
 Object.defineProperty(exports, "__esModule", { value: true });
+//
+// Array extension
+//
 Array.prototype.uniq = function (comparer) {
     return this.filter((value1, index1, array1) => {
         const index2 = array1.findIndex((value2) => comparer(value1, value2));
@@ -9,4 +12,20 @@ Array.prototype.uniq = function (comparer) {
 };
 Array.prototype.contains = function (value) {
     return (this.indexOf(value) >= 0);
+};
+Object.prototype.has = function (key) {
+    return Object.keys(this).contains(key);
+};
+Object.prototype.filterKeys = function (attributes) {
+    let ret = {};
+    const indexed = this.indexed();
+    Object.keys(this).forEach(key => {
+        if (attributes.contains(key)) {
+            ret[key] = indexed[key];
+        }
+    });
+    return ret;
+};
+Object.prototype.indexed = function () {
+    return this;
 };
