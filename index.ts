5c7d45ec2e981a80bf6c7b509ce40e47b4ea2df5
haruharuharuby <cocktail.house@gmail.com>
2019-10-15 17:55:08 +0200

    add object extension

--- index.ts
+++ index.ts
@@ -2,17 +2,24 @@
 
 export {}
 
+//
+// definition
+//
 declare global {
     interface Array<T> {
         uniq<T> (comparer: (value1: T, valu2: T) => boolean): T[];
         contains<T> (value: T): boolean;
-        findExactOne<T> (matcher: (value: T) => boolean): T | null;
     }
     interface Object {
         has (key: string): boolean;
+        filterKeys (keys: string[]): Object;
+        indexed (): IndexObject;
     }
 }
 
+//
+// Array extension
+//
 Array.prototype.uniq = function<T> (comparer: (value1: T, valu2: T) => boolean): T[] {
     return this.filter((value1, index1, array1): boolean => {
         const index2 = array1.findIndex((value2): boolean => comparer(value1, value2))
@@ -23,3 +30,32 @@ Array.prototype.uniq = function<T> (comparer: (value1: T, valu2: T) => boolean):
 Array.prototype.contains = function<T> (value: T): boolean {
     return (this.indexOf(value) >= 0)
 }
+
+//
+// Object extension
+//
+interface IndexObject {
+    [index: string]: string
+}
+
+Object.prototype.has = function (key: string): boolean {
+    return Object.keys(this).contains(key)
+}
+
+Object.prototype.filterKeys = function (attributes: string[]): Object {
+    let ret: IndexObject = {}
+    const indexed = this.indexed()
+    Object.keys(this).forEach(key => {
+        if (attributes.contains(key)) {
+            ret[key] = indexed[key]
+        }
+    })
+    return ret
+}
+
+Object.prototype.indexed = function (): IndexObject {
+    return this as IndexObject
+}
+
+
+
